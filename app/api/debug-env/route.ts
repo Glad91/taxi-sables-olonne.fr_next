1ff13a60b2cd8d145ff449b7895b678f75f3c2bf
Gladys TRUILLET <gladys.truillet@gmail.com>
2025-08-11 05:55:25 +0200

    debug: endpoint temporaire pour vérifier variables env

---
+++ app/api/debug-env/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  return NextResponse.json({
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    VERCEL_URL: process.env.VERCEL_URL,
+    headers: {
+      host: 'will be shown in request'
+    }
+  })
+}
